aa1afe384adb7ade5a9b0aa4e4efb1258e62f62a
Aleksandr <icealexxx@gmail.com>
2026-01-17 03:15:40 +0400

    feat: make share_with_children more generic

--- src/fd.rs
+++ src/fd.rs
@@ -1,16 +1,16 @@
 use crate::error::AppError;
 use rustix::io::fcntl_setfd;
-use std::os::fd::AsRawFd;
+use std::os::fd::{AsFd, AsRawFd};
 
 pub trait FileExtraFd {
     fn share_with_children(&self) -> Result<(), AppError>;
 }
 
-impl FileExtraFd for std::fs::File {
+impl<T: AsFd> FileExtraFd for T {
     fn share_with_children(&self) -> Result<(), AppError> {
         match fcntl_setfd(self, rustix::io::FdFlags::empty()) {
             Ok(_) => Ok(()),
-            Err(e) => Err(AppError::FileFdShare(self.as_raw_fd(), e)),
+            Err(e) => Err(AppError::FileFdShare(self.as_fd().as_raw_fd(), e)),
         }
     }
 }
