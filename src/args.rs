1466880dea00327b15e8a20e16d95ce593183c1c
Aleksandr <icealexxx@gmail.com>
2026-01-17 03:15:23 +0400

    feat: add InternalArgs parser

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,34 @@
 use crate::{error::AppError, utils};
-use lexopt::Parser;
-use std::{ffi::OsString, path::PathBuf};
+use lexopt::{Parser, ValueExt};
+use std::{
+    ffi::OsString,
+    os::fd::{FromRawFd, RawFd},
+    path::PathBuf,
+};
 
 const CONFIG_DIR: &str = "XDG_CONFIG_HOME";
+
+#[derive(Debug)]
+pub struct InternalArgs<I> {
+    pub ready_fd: RawFd,
+    pub args: I,
+}
+
+impl<I: Iterator<Item = OsString>> InternalArgs<I> {
+    pub fn from_iter(mut args: I) -> Result<Self, AppError> {
+        let internal = args.next().ok_or(AppError::BadArgs)?;
+        if internal != utils::SELF_INTERNAL_ARG {
+            tracing::error!("Unexpected first argument for internal launch: {internal:?}");
+            return Err(AppError::BadArgs);
+        }
+
+        let ready_fd = args.next().ok_or(AppError::BadArgs)?;
+        let ready_fd = ready_fd.parse::<i32>()?;
+        let ready_fd = unsafe { RawFd::from_raw_fd(ready_fd) };
+
+        Ok(Self { ready_fd, args })
+    }
+}
 
 #[derive(Debug)]
 pub struct Args {
@@ -23,7 +49,7 @@ impl Args {
         let mut rest = Vec::new();
         let mut is_app_image = false;
 
-        let mut parser = Parser::from_iter(iter);
+        let mut parser = Parser::from_args(iter);
         while let Some(arg) = parser.next()? {
             match arg {
                 Long("appimage") => is_app_image = true,
