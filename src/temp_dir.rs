ce287d3bb32f83ff689dd9ff9b10255494fef163
Aleksandr <icealexxx@gmail.com>
2026-01-18 19:57:17 +0400

    feat: add temp working dir for all temp files

---
+++ src/temp_dir.rs
@@ -0,0 +1,22 @@
+use crate::error::AppError;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct TempDirGuard {
+    dir: PathBuf,
+}
+
+impl TempDirGuard {
+    pub fn new(dir: PathBuf) -> Result<Self, AppError> {
+        std::fs::create_dir_all(&dir).map_err(|e| AppError::TempDir(dir.clone(), e))?;
+        Ok(Self { dir })
+    }
+}
+
+impl Drop for TempDirGuard {
+    fn drop(&mut self) {
+        if let Err(e) = std::fs::remove_dir_all(&self.dir) {
+            tracing::error!("Failed to remove sandbox temp dir: {e:?}")
+        }
+    }
+}
